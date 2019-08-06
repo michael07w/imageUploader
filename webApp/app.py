6bc48c16fc0f45abe5a229cfb51765f3adc5ea29
Michael Wessels <michaelwessels207@gmail.com>
2019-08-05 22:11:07 -0700

    Initial commit

---
+++ webApp/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+from PIL import Image
+import os
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route('/upload', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+
+        target = os.path.join(APP_ROOT, 'static/')    # file path to uploaded file
+
+        img = request.files['pic']    # uploaded FileStorage object
+        filename = img.filename    # file name of said object^^
+
+        destination = '/'.join([target, filename])
+        img.save(destination)    # saves uploaded file to dest "temp.jpg"
+
+        return render_template("displayUpload.html", image_name=filename)
+    else:
+        return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
